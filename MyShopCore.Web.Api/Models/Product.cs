36bbe7f5be697a726a696f48bc2215880785404a
Chamari Hansani <chamarih@cbctechsol.com>
2023-05-14 12:40:10 +0530

    added product and audit models

---
+++ MyShopCore.Web.Api/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace MyShopCore.Web.Api.Models
+{
+    public class Product:Audit
+    {
+        public Guid id { get; set; }
+        
+        public required string Title { get; set; }
+        public string Description { get; set; } 
+        public decimal CostPrice { get; set; }  
+
+        public decimal SellingPrice { get; set; }
+        public int OrderAfter { get; set; }
+
+        
+
+    }
+}
